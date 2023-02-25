5eddc3d5df12fac88fc3e7385dbec37d491572d5
Christian Queyrouze <queyrouzec@gmail.com>
2023-02-25 12:34:14 -0700

    init commit

---
+++ bin/dart_tree_sitter_bindings.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tree_sitter_bindings/dart_tree_sitter_bindings.dart' as dart_tree_sitter_bindings;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tree_sitter_bindings.calculate()}!');
+}
---
+++ test/dart_tree_sitter_bindings_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tree_sitter_bindings/dart_tree_sitter_bindings.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
